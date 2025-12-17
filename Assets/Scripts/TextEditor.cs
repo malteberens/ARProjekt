9e3b23ad3aa0fd6c477ba2a3c50d1dff6ba9128e
Malte Berens <220144796+malteberens@users.noreply.github.com>
2025-12-17 19:16:11 +0100

    Schuss counter

---
+++ Assets/Scripts/TextEditor.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using TMPro;
+
+public class TextEditor : MonoBehaviour
+{
+    public TMP_Text textField;  // Kann auch deaktiviertes GameObject referenzieren
+    
+    void Start()
+    {
+        // Funktioniert auch, wenn textField deaktiviert ist
+        if (textField != null)
+        {
+            textField.gameObject.SetActive(true);  // Erst aktivieren
+            textField.text = "Neuer Text";          // Dann bearbeiten
+        }
+    }
+}
